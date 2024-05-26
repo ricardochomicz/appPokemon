e760d6d8d15f20355e787d0650a07867ba89cc67
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-05-26 14:12:32 -0300

    update menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public appPages = [
     { title: 'Login', url: '/login', icon: 'mail' },
-    // { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
+    { title: 'Pokémon', url: '/pokemons', icon: 'paper-plane' },
     // { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
     // { title: 'Archived', url: '/folder/archived', icon: 'archive' },
     // { title: 'Trash', url: '/folder/trash', icon: 'trash' },
