be91d10d68c1380d67a106f5e9e788454c9a96b6
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-05-26 14:12:55 -0300

    import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
       BrowserModule,
       IonicModule.forRoot(),
       AppRoutingModule,
+      HttpClientModule
 
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
