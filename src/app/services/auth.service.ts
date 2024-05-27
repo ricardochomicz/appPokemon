0a741db9c82af79bf55cabbb1d9ff879b354389b
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-05-27 12:00:57 -0300

    autenticação firebaseui phone

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,8 +2,9 @@ import {Injectable} from '@angular/core';
 import firebase from "firebase/compat/app";
 import * as firebaseui from 'firebaseui'
 import {environment} from "../../environments/environment";
-
-
+import {getAuth} from "firebase/auth"
+import {BehaviorSubject, Observable} from "rxjs";
+import { Router } from '@angular/router';
 
 @Injectable({
     providedIn: 'root'
@@ -11,8 +12,9 @@ import {environment} from "../../environments/environment";
 export class AuthService {
 
     private ui: firebaseui.auth.AuthUI | null;
+    private userAuthenticatedSubject: BehaviorSubject<boolean>;
 
-    constructor() {
+    constructor(private router: Router) {
         const app = firebase.initializeApp(environment.firebaseConfig);
         const auth = firebase.auth()
 
@@ -23,22 +25,53 @@ export class AuthService {
         } else {
             this.ui = new firebaseui.auth.AuthUI(auth);
         }
+
+        this.userAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+
+        firebase.auth().onAuthStateChanged(user => {
+            this.userAuthenticatedSubject.next(!!user);
+        });
     }
 
     startFirebaseUi(elementId: string) {
         const uiConfig = {
             signInOptions: [
-                firebase.auth.EmailAuthProvider.PROVIDER_ID
+                {
+                    provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+                    recaptchaParameters: {
+                        type: 'image', // 'audio'
+                        size: 'normal', // 'invisible' or 'compact'
+                        badge: 'bottomleft' //' bottomright' or 'inline' applies to invisible.
+                    },
+                    defaultCountry: 'BR',
+                }
             ],
             callbacks: {
-                signInSuccessWithAuthResult: () => false
+                signInSuccessWithAuthResult: (authResult: any, redirectUrl: any) => {
+                    this.router.navigate(['/pokemon-favorites']); // Redireciona para a página de favoritos
+                    return false; // Retorna false para evitar o redirecionamento padrão do FirebaseUI
+                }
             }
         };
-
+        const auth = getAuth()
+        auth.useDeviceLanguage()
+        auth.languageCode = 'pt-BR'
+        firebase.auth().useDeviceLanguage()
         // @ts-ignore
         this.ui.start(elementId, uiConfig);
     }
 
+    isUserAuthenticated(): Observable<boolean> {
+        return this.userAuthenticatedSubject.asObservable();
+    }
+
+    logout() {
+        firebase.auth().signOut().then(() => {
+            this.router.navigate(['/pokemons']);
+        }).catch((error) => {
+            console.error('Error ao fazer o logout: ', error);
+        });
+    }
 
 
 }
