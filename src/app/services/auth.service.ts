4ab2b9a9bf44a88550310b924c538ba87ac68919
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-05-29 14:02:22 -0300

    remove authService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,19 +2,30 @@ import {Injectable} from '@angular/core';
 import firebase from "firebase/compat/app";
 import * as firebaseui from 'firebaseui'
 import {environment} from "../../environments/environment";
-import {getAuth} from "firebase/auth"
 import {BehaviorSubject, Observable} from "rxjs";
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
+import 'firebase/auth';
+import {AngularFirestore} from '@angular/fire/compat/firestore';
+import 'firebase/firestore';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {HttpClient} from "@angular/common/http";
+import {getAuth} from "firebase/auth";
+import User = firebase.User;
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
 
+    private currentUser: firebase.User | null = null;
     private ui: firebaseui.auth.AuthUI | null;
     private userAuthenticatedSubject: BehaviorSubject<boolean>;
 
-    constructor(private router: Router) {
+    constructor(
+        private router: Router,
+        private afAuth: AngularFireAuth,
+        private afs: AngularFirestore,
+        private httpClient: HttpClient) {
         const app = firebase.initializeApp(environment.firebaseConfig);
         const auth = firebase.auth()
 
@@ -26,6 +37,10 @@ export class AuthService {
             this.ui = new firebaseui.auth.AuthUI(auth);
         }
 
+        firebase.auth().onAuthStateChanged(user => {
+            this.currentUser = user;
+        });
+
         this.userAuthenticatedSubject = new BehaviorSubject<boolean>(false);
 
         firebase.auth().onAuthStateChanged(user => {
@@ -33,33 +48,46 @@ export class AuthService {
         });
     }
 
+
     startFirebaseUi(elementId: string) {
+        // Configuração da interface de usuário do Firebase Authentication (UI)
         const uiConfig = {
             signInOptions: [
                 {
                     provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-
-                    recaptchaParameters: {
-                        type: 'image', // 'audio'
-                        size: 'normal', // 'invisible' or 'compact'
-                        badge: 'bottomleft' //' bottomright' or 'inline' applies to invisible.
-                    },
                     defaultCountry: 'BR',
                 },
-                firebase.auth.EmailAuthProvider.PROVIDER_ID,
             ],
             callbacks: {
                 signInSuccessWithAuthResult: (authResult: any, redirectUrl: any) => {
-                    this.router.navigate(['/pokemon-favorites']); // Redireciona para a página de favoritos
+                    console.log(authResult)
+                    this.saveUserUidToSession()
+                    this.router.navigate(['/pokemons']);
+
                     return false; // Retorna false para evitar o redirecionamento padrão do FirebaseUI
                 }
             }
         };
-        firebase.auth().languageCode = 'pt-BR'
-        firebase.auth().useDeviceLanguage()
+
+        // Usa o idioma do dispositivo para a autenticação
+        firebase.auth().useDeviceLanguage();
+
+        // Inicia a interface de usuário do Firebase Authentication com a configuração fornecida
         // @ts-ignore
         this.ui.start(elementId, uiConfig);
     }
+
+    async saveUserUidToSession() {
+        const user = await this.afAuth.currentUser;
+        if (user) {
+            localStorage.setItem('userUid', user.uid);
+        }
+    }
+
+    getUserUidSession(){
+        return localStorage.getItem('userUid')
+    }
+
 
     isUserAuthenticated(): Observable<boolean> {
         return this.userAuthenticatedSubject.asObservable();
@@ -72,5 +100,4 @@ export class AuthService {
             console.error('Error ao fazer o logout: ', error);
         });
     }
-
 }
