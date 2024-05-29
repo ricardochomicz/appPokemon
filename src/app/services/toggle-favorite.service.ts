beef0d65a872f5e26f5a4a75bba045e7a4c962d4
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-05-29 14:10:20 -0300

    update toggle-favorite.service.ts

--- src/app/services/toggle-favorite.service.ts
+++ src/app/services/toggle-favorite.service.ts
@@ -1,30 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Pokemon} from "../models";
 import {FavoriteService} from "./favorite.service";
 import {ToastService} from "./toast.service";
 import {AuthService} from "./auth.service";
 import {Router} from "@angular/router";
+import firebase from "firebase/compat/app";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ToggleFavoriteService {
 
     pokemon: Pokemon | undefined;
     userAuthenticated = false;
-    constructor( private favoriteService: FavoriteService,
-                 private toastService: ToastService,
-                 private authService: AuthService,
-                 private router: Router) { }
+
+    constructor(private favoriteService: FavoriteService,
+                private toastService: ToastService,
+                private authService: AuthService,
+                private router: Router,
+                private afAuth: AngularFireAuth) {
+    }
 
     toggleFavorite(pokemon: Pokemon) {
         this.authService.isUserAuthenticated().subscribe(isAuthenticated => {
             this.userAuthenticated = isAuthenticated;
-            if(!this.userAuthenticated){
+            if (!this.userAuthenticated) {
                 this.toastService.toastMessage('Redirecionando para o login.');
                 this.router.navigate(['/login']);
-            }
-            else if (pokemon) {
+            } else {
                 if (this.isFavorite(pokemon.name)) {
                     this.favoriteService.removeFavorite(pokemon.name);
                     this.toastService.toastMessage('Pokémon removido dos favoritos!');
@@ -36,7 +40,11 @@ export class ToggleFavoriteService {
         });
     }
 
-    isFavorite(pokemonName: string): boolean {
-        return this.favoriteService.isFavorite(pokemonName);
+    isFavorite(pokemonName: string): any {
+        const user = this.authService.getUserUidSession()
+        // @ts-ignore
+        this.favoriteService.isFavorite(pokemonName, user);
     }
+
+
 }
