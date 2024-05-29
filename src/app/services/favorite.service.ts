2abae8a25830ff89500fd4207995c56f4c1d1b46
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-05-29 16:05:05 -0300

    update favorite.service.ts

--- src/app/services/favorite.service.ts
+++ src/app/services/favorite.service.ts
@@ -1,8 +1,11 @@
 import {Injectable} from '@angular/core';
 import {Pokemon} from "../models";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 import {AuthService} from "./auth.service";
 import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import firebase from "firebase/compat/app";
 
 @Injectable({
     providedIn: 'root'
@@ -16,13 +19,14 @@ export class FavoriteService {
 
     private favoritesSubject = new BehaviorSubject<number>(0);
 
-    constructor(private authService: AuthService, private afAuth: AngularFireAuth) {
-        this.afAuth.onAuthStateChanged(user => {
+    constructor(private authService: AuthService,
+                private afAuth: AngularFireAuth,
+                private http: HttpClient) {
+        const user = this.authService.getUserUidSession()
         if (user) {
-            this.loadFavorites(user.uid);
-            this.loadRatings();
+            this.loadFavorites(user);
+            this.loadRatings(user);
         }
-        });
     }
 
     addFavorite(pokemon: Pokemon) {
@@ -32,9 +36,9 @@ export class FavoriteService {
             if (!this.isFavorite(pokemon.name, user)) {
                 favorites.push(pokemon);
                 this.saveFavorites(user, favorites);
+                this.sendWebhook(pokemon, 'favorited')
+                this.favoritesSubject.next(favorites.length);
             }
-        } else {
-            console.error('Usuário não autenticado');
         }
     }
 
@@ -44,27 +48,29 @@ export class FavoriteService {
             let favorites = this.getFavorites(user);
             favorites = favorites.filter(fav => fav.name !== pokemonName);
             this.saveFavorites(user, favorites);
-        } else {
-            console.error('Usuário não autenticado');
+            this.favoritesSubject.next(favorites.length);
         }
     }
 
     getFavorites(user: string): Pokemon[] {
         const sessionKey = this.getSessionKey(this.storageKey, user);
         const favoritesJson = localStorage.getItem(sessionKey);
-        return favoritesJson ? JSON.parse(favoritesJson) : [];
+        const favorites = favoritesJson ? JSON.parse(favoritesJson) : [];
+        this.favoritesSubject.next(favorites.length); // Atualiza o BehaviorSubject
+        return favorites;
+
     }
 
     getFavoritesObservable() {
         return this.favoritesSubject.asObservable();
     }
 
-    isFavorite(pokemonName: string, user: string): boolean {
+    isFavorite(pokemonName: string, user: string): any {
         const favorites = this.getFavorites(user);
         return favorites.some(fav => fav.name === pokemonName);
     }
 
-    getFavoritesCount() {
+    getFavoritesCount(): Observable<number> {
         return this.favoritesSubject.asObservable();
     }
 
@@ -79,23 +85,22 @@ export class FavoriteService {
             const parsedRatings = JSON.parse(ratings);
             parsedRatings[pokemonName] = rating;
             localStorage.setItem(ratingsKey, JSON.stringify(parsedRatings));
-        } else {
-            console.error('Usuário não autenticado');
         }
     }
 
 
-    getRating(pokemonName: string): number {
-        const userUid = this.authService.getUserUidSession();
-        if (userUid) {
-            const ratingsKey = this.getSessionKey('ratings', userUid);
-            const ratings = localStorage.getItem(ratingsKey);
-            const parsedRatings = ratings ? JSON.parse(ratings) : {};
-            return parsedRatings[pokemonName] || 0;
-        } else {
-            console.error('Usuário não autenticado');
-            return 0;
+    getRating(pokemon: Pokemon): any {
+        const user = this.authService.getUserUidSession()
+        if (user) {
+            // const ratingsKey = this.getSessionKey('ratings', user);
+            // const ratings = localStorage.getItem(ratingsKey);
+            // const parsedRatings = ratings ? JSON.parse(ratings) : {};
+            // return parsedRatings[pokemonName] || 0;
+
+            this.loadRatings(user); // Ensure ratings are loaded
+            return this.ratings[pokemon.name] || 0;
         }
+
     }
 
 
@@ -112,19 +117,35 @@ export class FavoriteService {
         this.favoritesSubject.next(this.favorites.length);
     }
 
-    private loadRatings() {
-        const ratings = sessionStorage.getItem(this.ratingsKey);
-        this.ratings = ratings ? JSON.parse(ratings) : {};
-        const user = this.afAuth.currentUser;
-        if (user) {
-            // @ts-ignore
-            this.getFavorites(user.uid).forEach(pokemon => {
-                this.ratings[pokemon.name] = this.ratings[pokemon.name];
-            });
-        }
+    private loadRatings(uid: string) {
+
+        const sessionKey = this.getSessionKey('ratings', uid);
+        const ratingsJson = localStorage.getItem(sessionKey);
+        this.ratings = ratingsJson ? JSON.parse(ratingsJson) : {};
+
+        const favorites = this.getFavorites(uid);
+        favorites.forEach(pokemon => {
+            this.ratings[pokemon.name] = this.ratings[pokemon.name]
+        });
     }
+
 
     private getSessionKey(baseKey: string, uid: string): string {
         return `${baseKey}_${uid}`;
+    }
+
+    private sendWebhook(pokemon: Pokemon | { name: string }, action: string) {
+        const userUid = this.authService.getUserUidSession();
+        const webhookUrl = 'http://localhost:8080/webhook';
+        const payload = {
+            action: action,
+            pokemon: pokemon,
+            user: userUid
+        };
+        this.http.post(webhookUrl, payload).subscribe(response => {
+            console.log('Webhook sent:', response);
+        }, error => {
+            console.error('Error sending webhook:', error);
+        });
     }
 }
